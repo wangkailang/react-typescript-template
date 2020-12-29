3f36c919307e9e90a23c2414f6a9bfcc9fe87c4d
wangkailang <kailang@xsky.com>
2020-12-29 19:03:46 +0800

    feat: Add dependencies

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+/// <reference path="../node_modules/@types/webpack-env/index.d.ts" />
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
+
+module.hot && module.hot.accept();
