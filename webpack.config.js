436b3d681598e532a48f559d4382bc6a76d2ebef
wangkailang <kailang@xsky.com>
2020-12-30 10:47:44 +0800

    add analyze script

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,51 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src/index.tsx'),
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.(js|ts|tsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.ts', '.tsx']
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  plugins: [
+module.exports = webpackEnv => {
+  const isEnvAnalyze= webpackEnv.env === 'analyze';
+
+  const plugins = [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'index.html'),
-    })
-  ],
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    hot: true
+    }),
+  ];
+  if (isEnvProduction) {
+    plugins.push(new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: path.resolve(
+        __dirname,
+        'dist/analyze.html'
+      ),
+      openAnalyzer: false,
+    }))
+  }
+
+  return {
+    entry: path.resolve(__dirname, 'src/index.tsx'),
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(js|ts|tsx)$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.ts', '.tsx']
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    plugins,
+    devServer: {
+      contentBase: path.resolve(__dirname, 'dist'),
+      hot: true
+    }
   }
 };
